c3e7536ffefe72931ed03b31c70d81209c233abb
concreteface <concreteface@users.noreply.github.com>
2016-02-23 14:39:30 -0500

    cleaned up blackjack a bit

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -14,18 +14,15 @@ while player.calculate_hand < 21 && stand == false
   puts "Player score: #{player.calculate_hand}"
   puts "Hit or Stand? (H/S)"
   choice = gets.chomp
-
   if choice.downcase == 'h'
     player.hit
     num_cards += 1
     puts "Player was dealt #{player.cards[num_cards - 1].rank}#{player.cards[num_cards -1].suit}"
   elsif choice.downcase == 's'
-
     puts "Player score: #{player.calculate_hand}"
     stand = true
   end
 end
-
 if player.calculate_hand < 21
   computer.cards.each do |card|
     puts "Computer was dealt #{card.rank}#{card.suit}"
@@ -43,27 +40,7 @@ if player.calculate_hand < 21
   elsif computer.calculate_hand > player.calculate_hand
     puts "Sorry, you lose"
   end
-
 elsif player.calculate_hand == 21 && player.cards.length == 2
   puts "Blackjack! You win!"
 else puts "You've gone bust (you lose)."
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-# puts player.cards[0].rank
-# puts player.cards[1].rank
-# puts computer.cards[0].rank
-# puts computer.cards[1].rank
-# puts player.calculate_hand
-# puts computer.calculate_hand
