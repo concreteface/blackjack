14835f9f25ef78dc7ef6a5e8f474682ef1adb9f8
concreteface <concreteface@users.noreply.github.com>
2016-02-24 12:23:43 -0500

    competely classified blackjack!

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -12,53 +12,76 @@ class BlackJack
 
   def game
     puts 'Welcome to Blackjack!'
-    player.cards.each do |card|
-      puts "Player was dealt #{card.rank}#{card.suit}"
+    show_player_hand
+    hit_or_stand(2)
+    result_calculator
+  end
+end
+
+def computer_turn(player_score)
+  show_computer_hand
+  hit_or_stand_computer(2, player.calculate_hand)
+  puts "Computer score: #{computer.calculate_hand}"
+  result_calulator_computer
+end
+
+def result_calculator
+  if player.calculate_hand <= 21
+    if player.calculate_hand == 21 && player.cards.length == 2
+      puts "Blackjack! You win!"
+    elsif player.calculate_hand == 21
+      puts "You win!"
+    else
+      computer_turn(player.calculate_hand)
     end
-    num_cards = 2
-    stand = false
-    while player.calculate_hand < 21 && stand == false
+  elsif player.calculate_hand > 21
+    puts "You've gone bust (you lose)."
+  end
+end
+
+def result_calulator_computer
+  if computer.calculate_hand > 21
+    puts "The computer busted...you win!"
+  elsif computer.calculate_hand > player.calculate_hand
+    puts "Sorry, you lose"
+  end
+end
+
+def show_player_hand
+  player.cards.each do |card|
+    puts "Player was dealt #{card.rank}#{card.suit}"
+  end
+end
+
+def hit_or_stand(num_cards)
+  stand = false
+  while player.calculate_hand < 21 && stand == false
+    puts "Player score: #{player.calculate_hand}"
+    puts "Hit or Stand? (H/S)"
+    choice = gets.chomp
+    if choice.downcase == 'h'
+      player.hit
+      num_cards += 1
+      puts "Player was dealt #{player.cards[num_cards - 1].rank}#{player.cards[num_cards -1].suit}"
+    elsif choice.downcase == 's'
       puts "Player score: #{player.calculate_hand}"
-      puts "Hit or Stand? (H/S)"
-      choice = gets.chomp
-      if choice.downcase == 'h'
-        player.hit
-        num_cards += 1
-        puts "Player was dealt #{player.cards[num_cards - 1].rank}#{player.cards[num_cards -1].suit}"
-      elsif choice.downcase == 's'
-        puts "Player score: #{player.calculate_hand}"
-        stand = true
-      end
-    end
-    if player.calculate_hand <= 21
-
-      if player.calculate_hand == 21 && player.cards.length == 2
-        puts "Blackjack! You win!"
-      elsif player.calculate_hand == 21
-        puts "You win!"
-      else
-
-        computer.cards.each do |card|
-          puts "Computer was dealt #{card.rank}#{card.suit}"
-        end
-        computer_num_cards = 2
-        while computer.calculate_hand < 21 && computer.calculate_hand <= player.calculate_hand
-          puts "Computer score: #{computer.calculate_hand}"
-          computer.hit
-          computer_num_cards += 1
-          puts "Computer was dealt #{computer.cards[computer_num_cards - 1].rank}#{computer.cards[computer_num_cards -1].suit}"
-        end
-        puts "Computer score: #{computer.calculate_hand}"
-        if computer.calculate_hand > 21
-          puts "The computer busted...you win!"
-        elsif computer.calculate_hand > player.calculate_hand
-          puts "Sorry, you lose"
-        end
-      end
-    elsif player.calculate_hand > 21
-      puts "You've gone bust (you lose)."
+      stand = true
     end
   end
 end
 
-# BlackJack.new.game
+def show_computer_hand
+  computer.cards.each do |card|
+    puts "Computer was dealt #{card.rank}#{card.suit}"
+  end
+end
+
+def hit_or_stand_computer(computer_num_cards, player_score)
+  while computer.calculate_hand < 21 && computer.calculate_hand <= player_score
+    puts "Computer score: #{computer.calculate_hand}"
+    computer.hit
+    computer_num_cards += 1
+    puts "Computer was dealt #{computer.cards[computer_num_cards - 1].rank}#{computer.cards[computer_num_cards -1].suit}"
+  end
+end
+BlackJack.new.game
